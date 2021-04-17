7c4ed7632674f7149b2d2c2278030cc655779df9
Antony Gilo <giloantony@gmail.com>
2021-04-17 11:37:48 +0300

    Adding files to remote

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="">
+<div class="container">
+    <div class="row justify-content-center">
 
-
-    <script src="{{ asset('js/app.js') }}"></script>
+    </div>
 </div>
 @endsection
