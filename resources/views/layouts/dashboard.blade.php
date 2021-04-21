8acaf0f6fbbd016df4926ef22c271fec158c11cb
Antony Gilo <giloantony@gmail.com>
2021-04-21 18:37:36 +0300

    Styling the offcanvas-sidebar

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -32,8 +32,17 @@
 </head>
 <body>
     {{-- start of navbar --}}
-        <nav class="navbar navbar-expand-md navbar-dark shadow-sm" style="background-color: #2b4f60">
+        <nav class="navbar navbar-expand-md navbar-dark shadow-sm mb-0" style="background-color: #2b4f60">
             <div class="container-fluid">
+
+                {{-- off canvas trigger button --}}
+                <button class="navbar-toggler text-light" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">
+                    <span class="navbar-toggler-icon" data-bs-target="#offcanvasScrolling">
+
+                    </span>
+                </button>
+                {{-- end of off canvas trigger button --}}
+
                 <a class="navbar-brand text-light text-uppercase" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
@@ -87,6 +96,33 @@
 
     {{-- start of offcanvas --}}
 
+    <div class="offcanvas offcanvas-start sidebar-nav text-white" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Colored with scrolling</h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <p>Try scrolling the rest of the page to see this option in action.</p>
+      </div>
+    </div>
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasWithBackdrop" aria-labelledby="offcanvasWithBackdropLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasWithBackdropLabel">Offcanvas with backdrop</h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <p>.....</p>
+      </div>
+    </div>
+    <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Backdroped with scrolling</h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <p>Try scrolling the rest of the page to see this option in action.</p>
+      </div>
+    </div>
     {{-- end of off-canvas --}}
 
     <main class="py-4">
